1879d3cd6647f04a2186b92f81bfaf908a6c7076
Fazal ur Rehman <aghafazal5@gmail.com>
2021-05-06 19:31:05 +0500

    Update userPool.js

--- src/components/cognito/userPool.js
+++ src/components/cognito/userPool.js
@@ -1,7 +1,7 @@
 // npm i amazon-cognito-identity-js
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 const poolData = {
-  UserPoolId: process.env.REACT_APP_USER_POOL_ID, //find under general settings
-  ClientId: process.env.REACT_APP_CLIENT_ID, //find under general settings > App clients
+  UserPoolId: "us-east-2_03plzbY8x", //find under general settings
+  ClientId: "5ir2dkjltmui27jm4t4mo7qst", //find under general settings > App clients
 };
 export default new CognitoUserPool(poolData);
